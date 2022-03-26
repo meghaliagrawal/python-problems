d988bee3c29c7be14e8edb1f0090d360aecaca93
Meghali <meghali2892@gmail.com>
2022-03-26 18:59:51 +0530

    Add: One test file

---
+++ megtest1.py
@@ -0,0 +1,11 @@
+a = "saurabh"
+b = "meghali"
+c = "mishka"
+name = "saurabh"
+if name!= "saurabh":
+	print("Match {}".format(a))
+elif name == b:
+	print("Match {}".format(b))
+else:
+	print("Match {}".format(c))
+	
